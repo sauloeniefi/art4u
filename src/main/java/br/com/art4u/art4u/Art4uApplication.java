54abf3b299b2422941952eded010b8b9c4c8a50b
Saulo Oliveira <sauloeniefi@gmail.com>
2022-08-31 15:14:17 -0300

    Initial Commit

---
+++ src/main/java/br/com/art4u/art4u/Art4uApplication.java
@@ -0,0 +1,13 @@
+package br.com.art4u.art4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Art4uApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Art4uApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/art4u/art4u/Art4uApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.art4u.art4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Art4uApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
